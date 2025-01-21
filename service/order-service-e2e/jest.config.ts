21d64c2bb5c60d3e7e71c2a6a70422b20d212b6b
woojin-g <blueofficial0119@gmail.com>
2025-01-22 00:28:14 +0900

    [chore] NX를 이용한 프로젝트 구성

---
+++ service/order-service-e2e/jest.config.ts
@@ -0,0 +1,15 @@
+export default {
+  displayName: 'order-service-e2e',
+  preset: '../../jest.preset.js',
+  globalSetup: '<rootDir>/src/support/global-setup.ts',
+  globalTeardown: '<rootDir>/src/support/global-teardown.ts',
+  setupFiles: ['<rootDir>/src/support/test-setup.ts'],
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/order-service-e2e',
+};
